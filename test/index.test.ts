0ed8d3de1ec86ddfb62f8f1c38e0c14228f6fa7c
Daniel Starns <danielstarns@hotmail.com>
2020-08-15 11:46:09 +0100

    test: add functionally test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,13 @@
 import { expect } from "chai";
 import { describe } from "mocha";
 import envGQL from "../src";
+import gql from "graphql-tag";
+import path from "path";
+import fs from "fs";
+import {
+  constraintDirective,
+  constraintDirectiveTypeDefs,
+} from "graphql-constraint-directive";
 
 describe("envGQL", () => {
   describe("validation", () => {
@@ -90,6 +97,245 @@ describe("envGQL", () => {
             "Query, Mutation & Subscription not supported"
           );
         }
+      });
+    });
+  });
+
+  describe("functionally", () => {
+    describe("simple", () => {
+      it("should work with process.env", () => {
+        process.env.PORT = "4000";
+        process.env.URL = "http://github.com";
+        process.env.SECRET = "supersecret";
+        process.env.DEV = "false";
+
+        const typeDefs = `
+          input Config {
+            PORT: Int
+            URL: String
+            SECRET: String
+            DEV: Boolean
+          }
+        `;
+
+        const config = envGQL({
+          typeDefs: typeDefs,
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+
+        delete process.env.PORT;
+        delete process.env.URL;
+        delete process.env.SECRET;
+        delete process.env.DEV;
+      });
+
+      it("should throw PORT required", () => {
+        process.env.URL = "http://github.com";
+        process.env.SECRET = "supersecret";
+        process.env.DEV = "false";
+
+        const typeDefs = `
+          input Config {
+            PORT: Int!
+            URL: String
+            SECRET: String
+            DEV: Boolean
+          }
+        `;
+
+        try {
+          envGQL({
+            typeDefs,
+          });
+
+          throw new Error("I should not throw");
+        } catch (error) {
+          expect(error.message).to.include("env.PORT required");
+        }
+
+        delete process.env.URL;
+        delete process.env.SECRET;
+        delete process.env.DEV;
+      });
+
+      it("should work with override", () => {
+        const typeDefs = `
+          input Config {
+            PORT: Int
+            URL: String
+            SECRET: String
+            DEV: Boolean
+          }
+        `;
+
+        const config = envGQL({
+          typeDefs: typeDefs,
+          override: {
+            PORT: "4000",
+            URL: "http://github.com",
+            SECRET: "supersecret",
+            DEV: "false",
+          },
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+      });
+    });
+
+    describe("defaults", () => {
+      it("should assign correct defaults", () => {
+        const typeDefs = `
+          input Config {
+            PORT: Int = 4000
+            URL: String = "http://github.com"
+            SECRET: String = "supersecret"
+            DEV: Boolean = false
+          }
+      `;
+
+        const config = envGQL({
+          typeDefs: typeDefs,
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+      });
+    });
+
+    describe("graphql-tag", () => {
+      it("should work with graphql-tag", () => {
+        const typeDefs = gql`
+          input Config {
+            PORT: Int = 4000
+            URL: String = "http://github.com"
+            SECRET: String = "supersecret"
+            DEV: Boolean = false
+          }
+        `;
+
+        const config = envGQL({
+          typeDefs: typeDefs,
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+      });
+    });
+
+    describe(".gql file", () => {
+      it("should work with .gql file", () => {
+        process.env.PORT = "4000";
+        process.env.URL = "http://github.com";
+        process.env.SECRET = "supersecret";
+        process.env.DEV = "false";
+
+        const config = envGQL({
+          typeDefs: path.join(__dirname, "./.env.gql"),
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+
+        delete process.env.PORT;
+        delete process.env.URL;
+        delete process.env.SECRET;
+        delete process.env.DEV;
+      });
+    });
+
+    describe("directives", () => {
+      it("should work with correct directives", () => {
+        process.env.PORT = "4000";
+        process.env.URL = "http://github.com";
+        process.env.SECRET = "supersecret";
+        process.env.DEV = "false";
+
+        const typeDefs = `
+          input Config {
+            PORT: Int = 4000 @constraint(minLength: 4)
+            URL: String = "http://github.com" @constraint(format: "uri")
+            SECRET: String = "supersecret" @constraint(minLength: 5)
+            DEV: Boolean = false
+          }
+
+          ${constraintDirectiveTypeDefs}
+        `;
+
+        const config = envGQL({
+          typeDefs,
+          schemaTransforms: [constraintDirective()],
+        });
+
+        expect(config).to.deep.equal({
+          PORT: 4000,
+          URL: "http://github.com",
+          SECRET: "supersecret",
+          DEV: false,
+        });
+
+        delete process.env.PORT;
+        delete process.env.URL;
+        delete process.env.SECRET;
+        delete process.env.DEV;
+      });
+
+      it("should catch and throw a directive error", () => {
+        process.env.PORT = "4000";
+        process.env.URL = "not url";
+        process.env.SECRET = "supersecret";
+        process.env.DEV = "false";
+
+        const typeDefs = `
+          input Config {
+            PORT: Int = 4000 @constraint(minLength: 4)
+            URL: String = "http://github.com" @constraint(format: "uri")
+            SECRET: String = "supersecret" @constraint(minLength: 5)
+            DEV: Boolean = false
+          }
+
+          ${constraintDirectiveTypeDefs}
+        `;
+
+        try {
+          envGQL({
+            typeDefs,
+            schemaTransforms: [constraintDirective()],
+          });
+
+          throw new Error("I should not throw");
+        } catch (error) {
+          expect(error.message).to.include(
+            'Expected type "URL_String_format_uri"'
+          );
+        }
+
+        delete process.env.PORT;
+        delete process.env.URL;
+        delete process.env.SECRET;
+        delete process.env.DEV;
       });
     });
   });
